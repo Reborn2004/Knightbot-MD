5068e8dd00286e0e6116f28fe6b8a2f96a79c1a3
Reborn2004 <haalandgram@gmail.com>
2025-05-09 22:47:39 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const settings = {
   author: '‎',
   botName: "Momoshiki",
   botOwner: 'Reborn', // Your name
-  ownerNumber: '2347047552231', //Set your number here without + symbol, just add country code & number without any space
+  ownerNumber: '23407047552231', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
